9bd73c69a985d8f5583e30b68dc69c3a446c08ca
Samuel Lassonde <slassonde@gmail.com>
2023-02-16 22:16:45 -0500

    version 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 //import { AppContext } from '../context/AppContext';
 const Budget = () => {
     //const { budget } = useContext(AppContext);
     const [budget, setBudget] = useState('2000');
+    const { expenses } = useContext(AppContext)
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    if(budget > 20000){
+        alert("The value cannot exceed 20,000");
+        setBudget("20000")
+        return;
+    }
+    if(budget < totalExpenses ){
+       alert("You cannot reduce the budget lower than the spending");
+        setBudget("960")
+        return;
+    }
 
     return (
         <div className='alert alert-secondary'>

adb52395325abb8ff6de065381972fe891020407
Samuel Lassonde <slassonde@gmail.com>
2023-02-16 23:01:58 -0500

    c

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,6 +1,35 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext'; 
-const Currency = () => {
- 
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Currency = (props) => {
+
+    const [currency, setCurrency] = useState('');
+
+
+    const submitEvent = () => {
+
+
+    };
+
+    return (
+        <div>
+            <div className='row'>
+
+            <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+
+                    <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setCurrency(event.target.value)}>
+                        <option defaultValue>Choose...</option>
+                        <option value="$" name="Dollar"> $ Dollar</option>
+                        <option value="£" name="Pound">£ Pound</option>
+                        <option value="€" name="Euro">€ Euro</option>
+                        <option value="₹" name="Ruppee">₹ Ruppee</option>
+                    </select>
+
+                </div>
+                </div>
+
+        </div>
+    );
 };
+
 export default Currency;

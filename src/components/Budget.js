18fdd976c3eed210a20005617c78f1b4565decb1
Samuel Lassonde <slassonde@gmail.com>
2023-02-16 21:29:51 -0500

    EnhancedBudget version 1

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,21 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useState } from 'react';
+//import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    //const { budget } = useContext(AppContext);
+    const [budget, setBudget] = useState('2000');
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budget}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => setBudget(event.target.value)}>
+                </input>                 
+            </span>
         </div>
     );
 };
